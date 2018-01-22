170521bf9eb0b86bb63a18e782ce8e8e44cece5e
meng <1711621009@qq.com>
2018-01-22 15:54:25 +0800

    support platform setting

--- setting.py
+++ setting.py
@@ -2,8 +2,20 @@
 CREAt: 2018/1/19
 AUTHOR: Hehahutu
 """
+import sys
+
 DEBUG = False
 SECRET_KEY = 'SDF6(()%fln(%$)blh&$#hnmuk$hgk79g^$*n5JN'
 
-FILE_BASE_PATH = 'E:\\file_server\\TEST'
-# FILE_BASE_PATH = '/extend/fileserver'
+# 配置服务器读取文件根目录
+try:
+    env_platform = sys.platform
+    if env_platform == 'linux':
+        # linux 系统配置
+        FILE_BASE_PATH = '/extend/fileserver'
+    else:
+        # window 文件目录配置
+        FILE_BASE_PATH = 'E:\\file_server\\TEST'
+except:
+    # 获取异常，默认用Linux
+    FILE_BASE_PATH = '/extend/fileserver'
