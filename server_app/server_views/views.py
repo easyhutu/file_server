beb0e197c432a5038b489f94358c35d5c637b234
meng <1711621009@qq.com>
2018-01-22 20:22:50 +0800

    使用流式响应优化大文件下载

--- server_app/server_views/views.py
+++ server_app/server_views/views.py
@@ -4,11 +4,10 @@ AUTHOR: Hehahutu
 """
 
 from flask.views import MethodView
-from flask import render_template, make_response, session, request
+from flask import render_template, session, request, Response
 from server_app.helper.file_parser import get_file_list
 import urllib.parse
 from setting import BLANK_SIZE
-from flask import send_file
 
 
 class FileView(MethodView):
@@ -25,26 +24,27 @@ class FileView(MethodView):
                 return render_template('files.html', files=data[1], folders=data[2], last_path=last_path,
                                        last_url=last_url, path=request.path)
             elif data[0] == 'file':
-                # filename = data[1]
+                def poll_data(path):
+                    with open(path, 'rb') as f:
+                        while True:
+                            data = f.read(BLANK_SIZE)
+                            if data:
+                                yield data
+                            else:
+                                return False
+                filename = data[1]
                 path = data[2]
-                b_data = send_file(path)
-                # resp = make_response(b_data, 200)
-                # name = urllib.parse.quote(filename)
-                #
-                # resp.headers["Content-Disposition"] = f"attachment; filename*=utf-8''{name}"
-                # resp.headers["Content-Type"] = f"application/octet-stream; charset=utf-8"
-                # resp.headers["Connection"] = 'Keep-Alive'
+                b_data = poll_data(path)
+                resp = Response(b_data)
+                name = urllib.parse.quote(filename)
 
-                return b_data
+                resp.headers["Content-Disposition"] = f"attachment; filename*=utf-8''{name}"
+                resp.headers["Content-Type"] = f"application/octet-stream; charset=utf-8"
+                resp.headers["Connection"] = 'Keep-Alive'
+
+                return resp
         else:
             return 'URL错误,请检查路径是否正确!!!', 400
 
 
-def poll_data(path):
-    with open(path, 'rb') as f:
-        while True:
-            data = f.read(BLANK_SIZE)
-            if data:
-                yield data
-            else:
-                return None
+
